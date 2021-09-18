fc135f5ace1e9f5aeaed3426f43023fe5f744b38
Maxime Beugnet <maxime.beugnet@gmail.com>
2021-09-18 03:49:25 +0200

    first step with only React Native

---
+++ App.js
@@ -0,0 +1,90 @@
+import Realm from 'realm';
+import React, {useState} from 'react';
+import 'react-native-get-random-values';
+import {Button, ScrollView, StyleSheet, Text, TextInput, View,} from 'react-native';
+
+const Todos = ({todos, deleteTodo}) => {
+    console.log('TODOS ', todos)
+    return <ScrollView style={styles.todos}>
+        {todos.map(t => {
+            return <View key={t._id} style={styles.todo}>
+                <Text style={styles.text}>{t.text}</Text>
+                <Button color='red' title='X' onPress={() => deleteTodo(t._id)}/>
+            </View>
+        })}
+    </ScrollView>
+};
+
+const CreateTodo = ({addTodo}) => {
+    const [task, setTask] = useState("");
+
+    return <View style={styles.create}>
+        <TextInput style={styles.input} value={task} onChangeText={t => setTask(t)}/>
+        <Button title="+" onPress={() => addTodo(task)}/>
+    </View>
+};
+
+const App = () => {
+    const [todos, setTodos] = useState([])
+
+    const addTodo = (task) => {
+        console.log("Adding ", task);
+        let array = todos.slice();
+        array.push({_id: new Realm.BSON.ObjectID(), text: task, _partition: "Max"});
+        setTodos(array);
+    }
+
+    const deleteTodo = (id) => {
+        let array = todos.slice();
+        array = array.filter(t => t._id !== id);
+        setTodos(array);
+    }
+
+    return <View style={styles.container}>
+        <Todos todos={todos} deleteTodo={deleteTodo}/>
+        <CreateTodo addTodo={addTodo}/>
+    </View>
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    todos: {
+        marginTop: 20,
+        width: '100%',
+    },
+    todo: {
+        flexDirection: "row",
+        borderStyle: "solid",
+        borderColor: "blue",
+        borderWidth: 2,
+        borderRadius: 5,
+        justifyContent: "space-between",
+        margin: 10,
+        padding: 10
+    },
+    create: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        margin: 10,
+        padding: 10,
+        width: '100%'
+    },
+    text: {
+        fontSize: 25
+    },
+    input: {
+        borderWidth: 1,
+        borderRadius: 5,
+        width: '90%',
+        marginRight: 10,
+        padding: 5,
+        fontSize: 20
+    }
+});
+
+export default App;
---
+++ README.md
@@ -0,0 +1,30 @@
+# React Native Todo Sample App
+
+This application was developed for a live coding demo for WebExpo 2021.
+
+You can find the different steps in the different branches of this repo.
+
+# Some commands
+
+Set up the project
+
+```sh
+npx react-native init ReactNativeMaximeDemo
+cd ReactNativeMaximeDemo
+npm i realm
+npm i @realm.io/react
+npm i react-native-get-random-values
+```
+
+Start this bad boy (you will need an AVD)
+
+```sh
+# shell 1 - starts Metro
+npx react-native start
+# shell 2 - Compile the android app and starts it in the AVD
+npx react-native run-android
+```
+
+# Author
+
+Maxime Beugnet <maxime@mongodb.com>
